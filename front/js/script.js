67437b0765ce5ab86dca1e4b53cfc898eb18e9d4
Moreira <yoan.moreira.ud@gmail.com>
2023-08-10 16:15:21 +0200

    confiramtion

--- front/js/script.js
+++ front/js/script.js
@@ -1,8 +1,8 @@
 /**Recuperation de l'api */
 const url = "http://localhost:3000/api/products/";
-/**recuperation des élément */
+
 const container = document.getElementById("items");
-/**recuperation des informations */
+
 const getArticles = () => {
   fetch(url)
     .then(function (res) {
@@ -10,10 +10,10 @@ const getArticles = () => {
     })
     .then(function (data) {
       console.log(data)
-      /**creation d'une boucle */
+     
       for (product in data) {
         container.innerHTML +=
-        /**creation de nos fiche produit en récuperant  leurs donnée */
+      
           `<a href="./product.html?id=${data[product]._id}">
             <article>
               <img src="${data[product].imageUrl}"alt="${data[product].altTxt}">
